bdf921edbaba093930614469d814285c0c93e183
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-04-29 23:03:23 +0900

    Add test scene

---
+++ assets/tilesets/dev_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="dev_tileset" tilewidth="16" tileheight="16" tilecount="24" columns="4">
+ <image source="dev_tileset.png" trans="ff00ff" width="64" height="96"/>
+</tileset>
