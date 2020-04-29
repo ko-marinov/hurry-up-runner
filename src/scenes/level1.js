8b79fd65aee6fc9412b9e9db03f805074137ff1c
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-04-29 23:19:26 +0900

    Load test scene

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import tilesetImg from '../../assets/tilesets/dev_tileset.png';
 
 export class Level1 extends Phaser.Scene {
     constructor() {
@@ -6,9 +7,20 @@ export class Level1 extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('tileset', tilesetImg);
+        this.load.tilemapCSV('map', '../assets/tilemaps/level_1.csv');
     }
 
     create() {
+        this.map = this.make.tilemap({
+            key: 'map',
+            tileWidth: 16,
+            tileHeight: 16
+        });
+        let tileset = this.map.addTilesetImage('tileset');
+        let layer = this.map.createStaticLayer(0, tileset);
+        layer.y = this.game.config.height - layer.height;
+
         this.cameras.main.setBackgroundColor("#87ceeb");
     }
 }
