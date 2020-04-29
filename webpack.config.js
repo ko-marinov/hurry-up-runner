74c455d25e134bb7221271c3220266ede6c0e9fa
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-04-29 11:35:30 +0900

    Initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/i,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new ZipPlugin({
+            filename: "hurry-up-runner.zip",
+            exclude: /\.zip$/
+        })
+    ]
+};
