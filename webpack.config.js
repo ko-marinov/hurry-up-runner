4f611e2400d9b8084ff2a9568a0df3b2fbceaed3
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-05-10 13:22:08 +0900

    Set up typescript

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|ts)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-typescript']
+                    }
                 }
             },
             {
