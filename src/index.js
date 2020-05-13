7807ec8262e61749ebec6d0e4b9a91ef5e55850a
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-05-13 22:07:07 +0900

    Set config to produce a sharp image

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ var config = {
     type: Phaser.AUTO,
     width: 720,
     height: 400,
+    render: {
+        pixelArt: true
+    },
     physics: {
         default: "arcade",
         arcade: {
