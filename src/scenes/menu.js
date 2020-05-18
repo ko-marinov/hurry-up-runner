2289ec6b9da1d211b30e7e0f5b13fbe294da6356
Konstantin Marinov <konstantin.marinov@yahoo.com>
2020-05-18 20:47:28 +0900

    Support menu UI (#8)

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,11 +1,45 @@
 import 'phaser';
 
+import btnStartImg from '../../assets/images/button_start.png';
+import btnExitImg from '../../assets/images/button_exit.png';
+import btnRestartImg from '../../assets/images/button_restart.png';
+import btnNextLevelImg from '../../assets/images/button_next_level.png';
+import btnRepeatImg from '../../assets/images/button_repeat.png';
+import btnResumeImg from '../../assets/images/button_resume.png';
+import btnVolumeImg from '../../assets/images/button-volume.png';
+import titleImg from '../../assets/images/game_name.png';
+import scoreImg from '../../assets/images/total_score.png';
+
+const UI_TITLE_IMG = 'title';
+const UI_SCORE_IMG = 'score';
+const UI_BTN_START = 'btnStart';
+const UI_BTN_RESUME = 'btnResume';
+const UI_BTN_RESTART = 'btnRestart';
+const UI_BTN_NEXT_LEVEL = 'btnNextLevel';
+const UI_BTN_REPEAT = 'btnRepeat';
+const UI_BTN_EXIT = 'btnExit';
+const UI_BTN_TOGGLE_MUSIC = 'btnToggleMusic';
+
+const START_LAYOUT = 'start-layout';
+const PAUSE_LAYOUT = 'pause-layout';
+const FAIL_LAYOUT = 'fail-layout';
+const VICTORY_LAYOUT = 'victory-layout';
+
 export class MainMenu extends Phaser.Scene {
     constructor() {
         super("MainMenu");
     }
 
     preload() {
+        this.load.image('game-title', titleImg);
+        this.load.spritesheet('btn-start', btnStartImg, { frameWidth: 186, frameHeight: 40 });
+        this.load.spritesheet('btn-exit', btnExitImg, { frameWidth: 186, frameHeight: 40 });
+        this.load.spritesheet('btn-restart', btnRestartImg, { frameWidth: 186, frameHeight: 40 });
+        this.load.spritesheet('btn-next-level', btnNextLevelImg, { frameWidth: 186, frameHeight: 40 });
+        this.load.spritesheet('btn-repeat', btnRepeatImg, { frameWidth: 186, frameHeight: 40 });
+        this.load.spritesheet('btn-resume', btnResumeImg, { frameWidth: 186, frameHeight: 40 });
+        this.load.spritesheet('btn-volume', btnVolumeImg, { frameWidth: 40, frameHeight: 40 });
+        this.load.spritesheet('ui-score-image', scoreImg, { frameWidth: 252, frameHeight: 83 });
         this.load.audio('music-loop', '../../assets/sounds/music_loop.mp3');
     }
 
@@ -14,19 +48,33 @@ export class MainMenu extends Phaser.Scene {
         this.cameras.main.setBackgroundColor('rgba(0, 0, 0, 0.3)');
 
         let center_x = this.game.config.width / 2;
-        this.startButton = this.add.text(center_x, 100, 'START GAME');
-        this.startButton.setInteractive().on('pointerdown', this.startGameBtnEvent, this);
 
-        this.resumeButton = this.add.text(center_x, 100, 'RESUME GAME');
-        this.resumeButton.setInteractive().on('pointerdown', this.resumeGameBtnEvent, this);
+        // Init UI elements
+        this.uiElements = new Map();
+        this.uiElements.set(UI_TITLE_IMG, this.add.image(center_x, 100, 'game-title'));
+        this.uiElements.set(UI_SCORE_IMG, this.add.sprite(center_x, 90, 'ui-score-image'));
+        this.uiElements.set(UI_BTN_START, this.add.sprite(center_x, 200, 'btn-start').setInteractive());
+        this.uiElements.set(UI_BTN_RESUME, this.add.sprite(center_x, 200, 'btn-resume').setInteractive());
+        this.uiElements.set(UI_BTN_RESTART, this.add.sprite(center_x, 200, 'btn-restart').setInteractive());
+        this.uiElements.set(UI_BTN_EXIT, this.add.sprite(center_x, 200, 'btn-exit').setInteractive());
+        this.uiElements.set(UI_BTN_TOGGLE_MUSIC, this.add.sprite(center_x, 250, 'btn-volume').setInteractive());
 
-        this.restartButton = this.add.text(center_x, 100, 'RESTART LEVEL');
-        this.restartButton.setInteractive().on('pointerdown', this.restartLevelBtnEvent, this);
+        this.input.on('gameobjectover', this.onGameObjectOver, this);
+        this.input.on('gameobjectout', this.onGameObjectOut, this);
+        this.input.on('gameobjectdown', this.onGameObjectDown, this);
+        this.input.on('gameobjectup', this.onGameObjectUp, this);
 
-        this.toggleMusicButton = this.add.text(center_x, 150, 'MUSIC: ON');
-        this.toggleMusicButton.setInteractive().on('pointerdown', this.toggleMusicBtnEvent, this);
         this.input.keyboard.on("keyup_M", this.toggleMusic, this);
         this.isMuted = false;
+
+        this.pressedButton = undefined;
+
+        // Init screen layouts
+        this.layouts = new Map();
+        this.layouts.set(START_LAYOUT, { title: true, score: false, buttons: [UI_BTN_START, UI_BTN_TOGGLE_MUSIC], startY: 195 });
+        this.layouts.set(PAUSE_LAYOUT, { title: false, score: false, buttons: [UI_BTN_RESUME, UI_BTN_EXIT, UI_BTN_TOGGLE_MUSIC], startY: 125 });
+        this.layouts.set(FAIL_LAYOUT, { title: false, score: true, buttons: [UI_BTN_RESTART, UI_BTN_EXIT, UI_BTN_TOGGLE_MUSIC], startY: 160 });
+        this.layouts.set(VICTORY_LAYOUT, { title: false, score: true, buttons: [UI_BTN_NEXT_LEVEL, UI_BTN_REPEAT, UI_BTN_EXIT, UI_BTN_TOGGLE_MUSIC], startY: 160 });
 
         this.scene.bringToTop('MainMenu');
 
@@ -36,36 +84,98 @@ export class MainMenu extends Phaser.Scene {
         this.sound.play('music-loop', { loop: true });
     }
 
+    get(uiElementName) {
+        return this.uiElements.get(uiElementName);
+    }
+
+    setLayout(layoutName) {
+        let layout = this.layouts.get(layoutName);
+        if (layout.title) {
+            this.get(UI_TITLE_IMG).setVisible(true);
+        }
+        if (layout.score) {
+            this.get(UI_SCORE_IMG).setVisible(true);
+        }
+        let localY = layout.startY;
+        layout.buttons.forEach(btn => {
+            let uiBtn = this.get(btn);
+            uiBtn.y = localY;
+            uiBtn.setVisible(true);
+            localY += 45;
+        }, this);
+    }
+
+    onGameObjectOver(pointer, gameObject, event) {
+        if (gameObject === this.get(UI_BTN_TOGGLE_MUSIC)) {
+            gameObject.setFrame(this.isMuted ? 3 : 2);
+        } else {
+            gameObject.setFrame(1);
+        }
+    }
+
+    onGameObjectOut(pointer, gameObject, event) {
+        if (gameObject === this.get(UI_BTN_TOGGLE_MUSIC)) {
+            gameObject.setFrame(this.isMuted ? 1 : 0);
+        } else {
+            gameObject.setFrame(0);
+        }
+        this.pressedButton = undefined;
+    }
+
+    onGameObjectDown(pointer, gameObject, event) {
+        if (gameObject === this.get(UI_BTN_TOGGLE_MUSIC)) {
+            gameObject.setFrame(this.isMuted ? 5 : 4);
+        } else {
+            gameObject.setFrame(2);
+        }
+        this.pressedButton = gameObject;
+    }
+
+    onGameObjectUp(pointer, gameObject, event) {
+        let pressedButton = this.pressedButton;
+        this.pressedButton = undefined;
+
+        if (gameObject === this.get(UI_BTN_TOGGLE_MUSIC)) {
+            gameObject.setFrame(this.isMuted ? 3 : 2);
+        } else {
+            gameObject.setFrame(1);
+        }
+
+        if (pressedButton != gameObject) { return; }
+
+        if (gameObject === this.get(UI_BTN_START)) {
+            this.startGame(event);
+        } else if (gameObject === this.get(UI_BTN_RESUME)) {
+            this.resumeGame(event);
+        } else if (gameObject === this.get(UI_BTN_RESTART)) {
+            this.restartLevel(event);
+        } else if (gameObject === this.get(UI_BTN_TOGGLE_MUSIC)) {
+            this.toggleMusic(event);
+        }
+    }
+
     showStartScreen() {
         this.scene.setVisible(true, 'MainMenu');
-        this.startButton.setVisible(true);
-        this.toggleMusicButton.setVisible(true);
+        this.setLayout(START_LAYOUT);
         this.input.keyboard.on("keyup_ENTER", this.startGame, this);
     }
 
     showPauseScreen() {
         this.scene.setVisible(true, 'MainMenu');
-        this.resumeButton.setVisible(true);
-        this.toggleMusicButton.setVisible(true);
+        this.setLayout(PAUSE_LAYOUT);
         this.input.keyboard.on("keyup_ESC", this.resumeGame, this);
     }
 
     showFailScreen() {
         this.scene.setVisible(true, 'MainMenu');
-        this.restartButton.setVisible(true);
-        this.toggleMusicButton.setVisible(true);
+        this.setLayout(FAIL_LAYOUT);
         this.input.keyboard.on("keyup_ENTER", this.restartLevel, this);
     }
 
     hideAll() {
-        this.startButton.setVisible(false);
-        this.resumeButton.setVisible(false);
-        this.restartButton.setVisible(false);
-        this.toggleMusicButton.setVisible(false);
-    }
-
-    startGameBtnEvent(pointer, localX, localY, event) {
-        this.startGame(event);
+        this.uiElements.forEach(function (uiElem) {
+            uiElem.setVisible(false);
+        });
     }
 
     startGame(event) {
@@ -76,20 +186,12 @@ export class MainMenu extends Phaser.Scene {
         this.scene.get('Level1').start();
     }
 
-    resumeGameBtnEvent(pointer, localX, localY, event) {
-        this.resumeGame(event);
-    }
-
     resumeGame(event) {
         this.hideAll();
         event.stopPropagation();
         this.input.keyboard.off("keyup_ESC", this.resumeGame, this, false);
         this.scene.setVisible(false, 'MainMenu');
         this.scene.get('Level1').resume();
-    }
-
-    restartLevelBtnEvent(pointer, localX, localY, event) {
-        this.restartLevel(event);
     }
 
     restartLevel(event) {
@@ -100,16 +202,12 @@ export class MainMenu extends Phaser.Scene {
         this.scene.get('Level1').restart();
     }
 
-    toggleMusicBtnEvent(pointer, localX, localY, event) {
-        this.toggleMusic(event);
-    }
-
     toggleMusic(event) {
         event.stopPropagation();
         if (this.isMuted) {
-            this.toggleMusicButton.setText("MUSIC: ON");
+            this.get(UI_BTN_TOGGLE_MUSIC).setFrame(2);
         } else {
-            this.toggleMusicButton.setText("MUSIC: OFF");
+            this.get(UI_BTN_TOGGLE_MUSIC).setFrame(3);
         }
         this.isMuted = !this.isMuted;
         this.sound.setMute(this.isMuted);
