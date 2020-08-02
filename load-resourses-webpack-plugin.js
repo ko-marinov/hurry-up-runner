4480a1f863f11e14e59649205755bcbe152fa9a2
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-08-02 14:31:59 +0900

    Add webpack plugin for generate imports

---
+++ load-resourses-webpack-plugin.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+class LoadResourcesWebpackPlugin {
+    apply(compiler) {
+        let plugin = {
+            name: "LoadResourcesWebpackPlugin"
+        }
+        compiler.hooks.beforeRun.tap(plugin, (compiler) => {
+            let filename = './src/_resources.js';
+            let rcDirPath = 'assets/objects';
+            let resources = fs.readdirSync(rcDirPath);
+
+            let content = '';
+            resources.forEach((rc) => {
+                // cut file extension
+                let varName = rc.slice(0, rc.lastIndexOf('.'));
+                let rcImportItem = 'import ' + varName + ' from \'../' + rcDirPath + '/' + rc + '\';\n';
+                content += rcImportItem;
+            });
+
+            content += "\nexport function loadResources(scene) {\n";
+
+            // scene.load.image('air_conditioning', air_conditioning);
+            resources.forEach((rc) => {
+                // cut file extension
+                let varName = rc.slice(0, rc.lastIndexOf('.'));
+                let rcImportItem = '\tscene.load.image(\'' + varName + '\', ' + varName + ');\n';
+                content += rcImportItem;
+            });
+
+            content += "}\n";
+            fs.writeFileSync(filename, content);
+        });
+    }
+}
+
+module.exports = LoadResourcesWebpackPlugin;
